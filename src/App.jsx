c22a8ddc792a9b008e991123e4d5350857ff6cdd
dalelantowork <dalelanto.work@gmail.com>
2021-04-20 20:06:53 +0800

    add dark mode

--- src/App.jsx
+++ src/App.jsx
@@ -1,50 +1,75 @@
-import React from 'react'
-import { Typography, AppBar,Button, Card, CardActions, CardContent, CardMedia, CssBaseline, Grid, Toolbar, Container } from '@material-ui/core'
+import React , { useState }from 'react'
+import { Switch, Typography, AppBar,Button, Card, CardActions, CardContent, CardMedia, Grid, Toolbar, Container, Paper } from '@material-ui/core'
 import { PhotoCamera } from '@material-ui/icons'
-import { Photo } from '@material-ui/icons'
-
+import { Brightness5,Brightness4 } from '@material-ui/icons'
+import { ThemeProvider, createMuiTheme } from '@material-ui/core/styles'
 import useStyles from './styles'
 
 const cards = [1,2,3,4,5,6,7,8,9]
 
 const App = () => {
     const classes= useStyles()
+    const [darkMode, setDarkMode] = useState(false);
+
+      
+    const darkTheme = createMuiTheme({
+        palette: {
+            type: "dark",
+            primary: {
+                main: '#ffc400',
+              },
+              secondary: {
+                main: '#c6ff00',
+              },
+              
+        },
+    });
+
+    const lightTheme = createMuiTheme({
+        palette: {
+            type: "light",
+        },
+    });
 
     return (
-        <>
-        <CssBaseline>
-            <AppBar position="relative">
+        <ThemeProvider theme={darkMode ? darkTheme : lightTheme }>
+        <Paper className={darkMode ? classes.paperdark : classes.paperlight}>
+            <AppBar  position="relative" className={darkMode ? classes.appbardark : classes.appbarlight} >
                 <Toolbar>
                     <PhotoCamera className={classes.icon}/>
                     <Typography variant="h6">
                         Image Gallery
                     </Typography>
+                    <Switch checked={darkMode} onChange={()=>setDarkMode(!darkMode)} className={classes.right} />
+                    {darkMode? <Brightness4 /> :  <Brightness5 />}
                 </Toolbar>
             </AppBar>
             <main>
                 <div className={classes.container}>
-                    <Container maxWidth="sm">
-                        <Typography variant="h2" align="center" color="textPrimary" gutterBottom>
+                    <div className={darkMode ? classes.overlaydark : classes.overlaylight }>
+                    <Container maxWidth="sm" className={classes.containerText}>
+                        <Typography variant="h2" align="center" color="textPrimary" className={classes.imageGalleryText} gutterBottom>
                             Image Gallery
                         </Typography>
-                        <Typography variant="h5" align="center" color="textSecondary" paragraph>
-                            Hello everyone This is an image gallery and I'm trying to make this sentenct as long as possible so we can see how does it look like on the screen.
+                        <Typography variant="h5" align="center" color="textSecondary" className={classes.imageParagText} paragraph>
+                            Hello everyone This is an image gallery and I'm trying to make this sentence as long as possible so we can see how does it look like on the screen.
                         </Typography>
                         <div className={classes.buttons}>
                             <Grid container spacing={2} justify="center" >
                                 <Grid item>
-                                    <Button variant="contained" color="primary">
+                                    <Button variant="contained" color="primary" className={darkMode ? classes.buttondark : classes.buttonlight }>
                                         See my photos
                                     </Button>
                                 </Grid>
                                 <Grid item>
-                                    <Button variant="outlined" color="primary">
+                                    <Button variant="outlined" color="primary" >
                                         Secondary action
                                     </Button>
                                 </Grid>
                             </Grid>
                         </div>
                     </Container>
+                    </div>
                 </div>
                 <Container className={classes.cardGrid} maxWidth="md">
                     <Grid container spacing={4} justify="center">
@@ -65,8 +90,8 @@ const App = () => {
                                     </Typography>
                                 </CardContent>
                                 <CardActions>
-                                    <Button size="small" color="primary">View</Button>
-                                    <Button size="small" color="secondary">Edit</Button>
+                                    <Button size="small" color="secondary">View</Button>
+                                    <Button size="small" color="primary">Edit</Button>
                                 </CardActions>
                             </Card>
                         </Grid>
@@ -83,8 +108,8 @@ const App = () => {
                     Made by Dale Lanto for the showcase of a photo album design made from ReactJS and Material-UI
                 </Typography>
             </footer>
-        </CssBaseline>
-        </>
+        </Paper>
+        </ThemeProvider>
     )
 }
 
--- src/styles.js
+++ src/styles.js
@@ -1,9 +1,44 @@
 import { makeStyles } from '@material-ui/core/styles'
+import camera from './images/camera.jpg'
+
 
 const useStyles = makeStyles((theme)=>({
     container: {
-        backgroundColor: theme.palette.background.paper,
-        padding: theme.spacing(8, 0 ,6)
+        //backgroundColor: theme.palette.background.paper,
+        backgroundImage: `url(${camera})`,
+        backgroundSize: 'cover',
+        backgroundPosition: 'center',
+        
+        position: 'relative',
+        objectFit: 'cover',
+        width: '100%',
+        height: 400,
+        margin: 0,
+    },
+    overlaylight: {
+        width: '100%',
+        height: '100%',
+        margin: 0,
+        backgroundColor: 'rgba(255,255,255,0.8)',
+        justify: 'center',
+    },
+    overlaydark: {
+        width: '100%',
+        height: '100%',
+        margin: 0,
+        backgroundColor: 'rgba(0,0,0,0.8)',
+        justify: 'center',
+    },
+    containerText: {
+        padding: theme.spacing(8, 0 ,6),
+    },
+    imageGalleryText: {
+        //color: '#ffffff'
+        padding: '0 15px;'
+    },
+    imageParagText: {
+        //color: '#e6e6e6'
+        padding: '0 15px;'
     },
     icon: {
         marginRight: '20px'
@@ -13,6 +48,7 @@ const useStyles = makeStyles((theme)=>({
     },
     cardGrid: {
         padding: '20px 0'
+        
     },
     card: {
         height: '100%',
@@ -26,8 +62,44 @@ const useStyles = makeStyles((theme)=>({
         flexGrow: 1,
     },
     footer: {
-        backgroundColor: theme.palette.background.paper,
+        //backgroundColor: theme.palette.background.paper,
         padding: '50px 0'
+    },
+    appbarlight: {
+        //backgroundColor: '#000000',
+        //backgroundColor: '#ffff00 !important',
+        // backgroundImage: `url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='100%25' height='100%25' viewBox='0 0 1200 800'%3E%3Cdefs%3E%3CradialGradient id='a' cx='0' cy='800' r='800' gradientUnits='userSpaceOnUse'%3E%3Cstop offset='0' stop-color='%23ff8000'/%3E%3Cstop offset='1' stop-color='%23ff8000' stop-opacity='0'/%3E%3C/radialGradient%3E%3CradialGradient id='b' cx='1200' cy='800' r='800' gradientUnits='userSpaceOnUse'%3E%3Cstop offset='0' stop-color='%2300ff19'/%3E%3Cstop offset='1' stop-color='%2300ff19' stop-opacity='0'/%3E%3C/radialGradient%3E%3CradialGradient id='c' cx='600' cy='0' r='600' gradientUnits='userSpaceOnUse'%3E%3Cstop offset='0' stop-color='%239900ff'/%3E%3Cstop offset='1' stop-color='%239900ff' stop-opacity='0'/%3E%3C/radialGradient%3E%3CradialGradient id='d' cx='600' cy='800' r='600' gradientUnits='userSpaceOnUse'%3E%3Cstop offset='0' stop-color='%23ffff00'/%3E%3Cstop offset='1' stop-color='%23ffff00' stop-opacity='0'/%3E%3C/radialGradient%3E%3CradialGradient id='e' cx='0' cy='0' r='800' gradientUnits='userSpaceOnUse'%3E%3Cstop offset='0' stop-color='%23FF0000'/%3E%3Cstop offset='1' stop-color='%23FF0000' stop-opacity='0'/%3E%3C/radialGradient%3E%3CradialGradient id='f' cx='1200' cy='0' r='800' gradientUnits='userSpaceOnUse'%3E%3Cstop offset='0' stop-color='%230CF'/%3E%3Cstop offset='1' stop-color='%230CF' stop-opacity='0'/%3E%3C/radialGradient%3E%3C/defs%3E%3Crect fill='url(%23a)' width='1200' height='800'/%3E%3Crect fill='url(%23b)' width='1200' height='800'/%3E%3Crect fill='url(%23c)' width='1200' height='800'/%3E%3Crect fill='url(%23d)' width='1200' height='800'/%3E%3Crect fill='url(%23e)' width='1200' height='800'/%3E%3Crect fill='url(%23f)' width='1200' height='800'/%3E%3C/svg%3E")`,
+        // backgroundAttachment: 'fixed',
+        // backgroundSize: 'cover',
+    },
+    appbardark: {
+        backgroundColor: '#000000',
+        color: '#ffffff',
+        // backgroundImage: `url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='100%25' height='100%25' viewBox='0 0 1200 800'%3E%3Cdefs%3E%3CradialGradient id='a' cx='0' cy='800' r='800' gradientUnits='userSpaceOnUse'%3E%3Cstop offset='0' stop-color='%23ff8000'/%3E%3Cstop offset='1' stop-color='%23ff8000' stop-opacity='0'/%3E%3C/radialGradient%3E%3CradialGradient id='b' cx='1200' cy='800' r='800' gradientUnits='userSpaceOnUse'%3E%3Cstop offset='0' stop-color='%2300ff19'/%3E%3Cstop offset='1' stop-color='%2300ff19' stop-opacity='0'/%3E%3C/radialGradient%3E%3CradialGradient id='c' cx='600' cy='0' r='600' gradientUnits='userSpaceOnUse'%3E%3Cstop offset='0' stop-color='%239900ff'/%3E%3Cstop offset='1' stop-color='%239900ff' stop-opacity='0'/%3E%3C/radialGradient%3E%3CradialGradient id='d' cx='600' cy='800' r='600' gradientUnits='userSpaceOnUse'%3E%3Cstop offset='0' stop-color='%23ffff00'/%3E%3Cstop offset='1' stop-color='%23ffff00' stop-opacity='0'/%3E%3C/radialGradient%3E%3CradialGradient id='e' cx='0' cy='0' r='800' gradientUnits='userSpaceOnUse'%3E%3Cstop offset='0' stop-color='%23FF0000'/%3E%3Cstop offset='1' stop-color='%23FF0000' stop-opacity='0'/%3E%3C/radialGradient%3E%3CradialGradient id='f' cx='1200' cy='0' r='800' gradientUnits='userSpaceOnUse'%3E%3Cstop offset='0' stop-color='%230CF'/%3E%3Cstop offset='1' stop-color='%230CF' stop-opacity='0'/%3E%3C/radialGradient%3E%3C/defs%3E%3Crect fill='url(%23a)' width='1200' height='800'/%3E%3Crect fill='url(%23b)' width='1200' height='800'/%3E%3Crect fill='url(%23c)' width='1200' height='800'/%3E%3Crect fill='url(%23d)' width='1200' height='800'/%3E%3Crect fill='url(%23e)' width='1200' height='800'/%3E%3Crect fill='url(%23f)' width='1200' height='800'/%3E%3C/svg%3E")`,
+        // backgroundAttachment: 'fixed',
+        // backgroundSize: 'cover',
+    },
+    buttondark: {
+        backgroundColor: '#388e3c',
+        color: '#ffffff',
+    },
+    buttonlight: {
+        backgroundColor: '#3f51b5',
+    },
+    switchdark: {
+        color: '#c6ff00 !important',
+    },
+    switchlight: {
+        
+    },
+    right: {
+        marginLeft: 'auto'
+    },
+    paperdark: {
+        backgroundColor: '#333333',
+    },
+    paperlight: {
+        backgroundColor: '#F5F5F5',
     },
 }))
 
